63dcd084521500798610328905c44ea12f074dcb
ryota-murakami <dojce1048@gmail.com>
2023-03-09 11:40:02 +0900

    add signal/effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { createSignal, createEffect } from 'solid-js';
+
 import logo from './logo.svg';
 import styles from './App.module.css';
 
 function App() {
+  const [first, setFirst] = createSignal("JSON");
+const [last, setLast] = createSignal("Bourne");
+
+createEffect(() => console.log(`${first()} ${last()}`));
   return (
     <div class={styles.App}>
       <header class={styles.header}>
